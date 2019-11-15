b5255911d9c997cc9d0b9f4899de34f5310c6428
vvalmeidas <vvalmeida96@gmail.com>
2019-11-15 20:51:11 -0300

    Testing dynamoDB integration

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 var read = require('./read_tweets');
 
+var AWS = require("aws-sdk");
+
+var dynamodb = new AWS.DynamoDB({ region: 'sa-east-1' });
+dynamodb.batchGetItem(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else console.log(data); // successful response
+});
+
+
 
 async function iniciar() {
+    /*
     while (true) {
         read.start();
         console.log("oi");
         await sleep(60000);
     }
+    */
 }
 
 
