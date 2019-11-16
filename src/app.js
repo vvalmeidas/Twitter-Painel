6230add165b1e99b16cae958f35d7b37285a5eef
vvalmeidas <vvalmeida96@gmail.com>
2019-11-15 21:10:57 -0300

    Testing dynamoDB

--- src/app.js
+++ src/app.js
@@ -2,6 +2,39 @@ var read = require('./read_tweets');
 
 var AWS = require("aws-sdk");
 
+var params = {
+    RequestItems: {
+        "Music": {
+            Keys: [{
+                    "Artist": {
+                        S: "No One You Know"
+                    },
+                    "SongTitle": {
+                        S: "Call Me Today"
+                    }
+                },
+                {
+                    "Artist": {
+                        S: "Acme Band"
+                    },
+                    "SongTitle": {
+                        S: "Happy Day"
+                    }
+                },
+                {
+                    "Artist": {
+                        S: "No One You Know"
+                    },
+                    "SongTitle": {
+                        S: "Scared of My Shadow"
+                    }
+                }
+            ],
+            ProjectionExpression: "AlbumTitle"
+        }
+    }
+};
+
 var dynamodb = new AWS.DynamoDB({ region: 'sa-east-1' });
 dynamodb.batchGetItem(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
