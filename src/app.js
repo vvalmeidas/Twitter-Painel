7ff285ec0ef9c5496e18ad8e37f58520cb60c5e3
vvalmeidas <vvalmeida96@gmail.com>
2019-11-21 22:56:33 -0300

    Updating keys

--- src/app.js
+++ src/app.js
@@ -4,30 +4,30 @@ var monitoring = require('./monitoring');
 var util = require('./util');
 
 async function iniciar() {
+    /* 
+        var data = {};
 
-    var data = {};
-
-    data.id = '1196895283734614019';
-    data.user = 'GumballUniverso',
-        data.date = '19/11/2019',
-        data.time = '17:57:27',
-        data.text = 'Dá uma olhada na mulherada que vai cantar no #AMAs2019 \n' +
-        '\n' +
-        'Toni Braxton\n' +
-        'Shania Twain\n' +
-        'Christina Aguilera\n' +
-        'Kesha\n' +
-        'Selena Gomez\n' +
-        'Taylor Swift\n' +
-        'Camila Cabello\n' +
-        'Dua Lipa\n' +
-        'Billie Eilish\n' +
-        'Lizzo https://t.co/QRcTkYV0o0',
-        data.isRT = 'true'
+        data.id = '119689528370034614019';
+        data.user = 'GumballUniverso',
+            data.date = '19/11/2019',
+            data.time = '17:57:27',
+            data.text = 'Dá uma olhada na mulherada que vai cantar no #AMAs2019 \n' +
+            '\n' +
+            'Toni Braxton\n' +
+            'Shania Twain\n' +
+            'Christina Aguilera\n' +
+            'Kesha\n' +
+            'Selena Gomez\n' +
+            'Taylor Swift\n' +
+            'Camila Cabello\n' +
+            'Dua Lipa\n' +
+            'Billie Eilish\n' +
+            'Lizzo https://t.co/QRcTkYV0o0',
+            data.isRT = 'true' */
 
 
-    dynamo.saveData("tweets", data);
-    //tweet.search();
+    //dynamo.saveData("tweets", data);
+    tweet.search();
     /* while (true) {
         
         await sleep(60000);
